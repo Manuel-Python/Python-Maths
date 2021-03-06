22f1397099767e3c58b58bad9a85bfd81fbd1f40
Manuel Carvalho <burrowsrd@gmail.com>
2021-03-06 21:02:22 +1100

    Initial Commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+# Based on Angela Yu's work
+
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+
+window = Tk()
+window.title("Maths")
+window.config(padx=200, pady=200, bg=GREEN)
+
+window.mainloop()
