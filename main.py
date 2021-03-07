046619123b085877c8fc33f103f1a55d672a5058
Manuel Carvalho <burrowsrd@gmail.com>
2021-03-07 16:42:30 +1100

    Added pheta (1)

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import math
 from tkinter import *
 
 # Based on Angela Yu's work
@@ -11,5 +12,8 @@ FONT_NAME = "Courier"
 window = Tk()
 window.title("Maths")
 window.config(padx=200, pady=200, bg=GREEN)
+
+pheta = 1 / (2 * math.sin(math.radians(18)))
+print(pheta)
 
 window.mainloop()
