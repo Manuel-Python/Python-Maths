a71d3c30be2cd64134b64d0fb1ad1073508342c2
Manuel Carvalho <burrowsrd@gmail.com>
2021-03-09 13:16:48 +1100

    Added pheta ( 5,6)

--- main.py
+++ main.py
@@ -14,15 +14,24 @@ window.title("Maths")
 window.config(padx=200, pady=200, bg=GREEN)
 
 pheta = 1 / (2 * math.sin(math.radians(18)))
-print(pheta)
+print("18 - ", pheta)
 
 pheta1 = 2 * math.cos(math.radians(36))
-print(pheta1)
+print("36 - ", pheta1)
 
 pheta2 = 2 * math.sin(math.radians(54))
-print(pheta2)
+print("54 - ", pheta2)
 
 pheta3 = 1 / (2 * math.cos(math.radians(72)))
-print(pheta3)
+print("72 - ", pheta3)
+
+pheta4 = (1 / (math.sin(math.radians(18))) / 2)
+print("18 - ", pheta4)
+
+pheta5 = (1 / (math.cos(math.radians(72))) / 2)
+print("72 - ", pheta5)
+
+pheta6 = 2 / (1 / (math.sin(math.radians(54))))
+print("54 - ", pheta6)
 
 window.mainloop()
